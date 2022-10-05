d359d803a5a055b6d39c7d84eb61b0aacc52e056
andriusmw <andriusmw@yahoo.es>
2022-10-05 12:00:37 +0200

    cadena de conexión con DB en y ContactoController

---
+++ ProyectoCrud/Controllers/ContactoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoCrud.Controllers
+{
+    public class ContactoController : Controller
+    {
+        // GET: Contacto
+        //crea una cadena de conexión para conectarte a SQL server
+
+        private static string conexion = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+        //Se conecta al archivo Web.config
+
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
